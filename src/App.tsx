6a2984aeae33d4ffc4698eef9bcfecf2ff3e1813
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-12-16 00:13:50 +1100

    Created positioning of todolist front and center of screen. Components yet to be seperated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import logo from './logo.svg'
+import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Container'
+import styled from '@emotion/styled'
 import './App.css'
+
+const StyledTodoListContainer = styled(Grid)`
+    height: 100vh;
+`
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Container className="HomeContainer">
+            <StyledTodoListContainer
+                className="LayoutContainer"
+                display={'flex'}
+                xs={12}
+                justifyContent={'center'}
+                alignItems={'center'}
+            >
+                <Grid item>
+                    <p>hello</p>
+                </Grid>
+            </StyledTodoListContainer>
+        </Container>
     )
 }
 
